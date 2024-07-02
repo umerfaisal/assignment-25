b1ea966287dcc548d56df837d6769956ac39d99a
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-07-02 18:10:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Define variable
+var alienColor = "Black";
+if (alienColor === "Black") {
+    // code block
+    console.log("The player just earned 5 points.");
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+//Define variable
+let alienColor = "White";
+
+if( alienColor === "Black"){
+    // code block
+    console.log("Alien color is black The player just earned 5 points.");
+}
+
+if( alienColor === "White"){
+    // code block
+    console.log("Alien color is White The player just earned 5 points.");
+}
